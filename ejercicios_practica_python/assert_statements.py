6fcc15fabc10df67f23b72b2bcb08596948dff9b
Fabian Tovar <thewolf3shadow@gmail.com>
2021-09-28 14:57:43 -0500

    conectando el repositorio

---
+++ ejercicios_practica_python/assert_statements.py
@@ -0,0 +1,20 @@
+"""
+pide al usuario un numero y encuentra los divisores del numero
+"""
+def divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+
+def run():
+    num = input('Ingresa un número: ')
+    assert num.isnumeric(), "debes ingresar un número"
+    print(divisors(int(num)))
+    print("Termino mi programa")
+    
+
+if __name__ == '__main__':
+    run()
