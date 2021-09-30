f237fc26f3fa01391b1834afc8182a53075d23c5
Fabian Tovar <thewolf3shadow@gmail.com>
2021-09-30 15:52:41 -0500

    se modifico el orden del data set y se agrego un blog de notas con el link y descripcion de donde se realizara los ejercicios

--- ejercicios_practica_python/list_and_dicts.py
+++ ejercicios_practica_python/list_and_dicts.py
@@ -1,26 +1,26 @@
 def run():
-    # my_list = [1, "Hello", True, 4.5]
-    # my_dict = {"firstname": "Facundo", "lastname": "García"}
+    my_list = [1, "Hello", True, 4.5]
+    my_dict = {"firstname": "Facundo", "lastname": "García"}
 
-    # super_list = [
-    #     {"firstname": "Facundo", "lastname": "García"},
-    #     {"firstname": "Miguel", "lastname": "Rodriguez"},
-    #     {"firstname": "Pablo", "lastname": "Trinidad"},
-    #     {"firstname": "Susana", "lastname": "Martinez"},
-    #     {"firstname": "José", "lastname": "Fernandez"},
-    # ]
+    super_list = [
+        {"firstname": "Facundo", "lastname": "García"},
+        {"firstname": "Miguel", "lastname": "Rodriguez"},
+        {"firstname": "Pablo", "lastname": "Trinidad"},
+        {"firstname": "Susana", "lastname": "Martinez"},
+        {"firstname": "José", "lastname": "Fernandez"},
+     ]
 
-    # super_dict = {
-    #     "natural_nums": [1, 2, 3, 4, 5],
-    #     "integer_nums": [-1, -2, 3, 0, 1],
-    #     "floating_nums": [1.1, 4.55, 6.43],
-    # }
+    super_dict = {
+        "natural_nums": [1, 2, 3, 4, 5],
+        "integer_nums": [-1, -2, 3, 0, 1],
+        "floating_nums": [1.1, 4.55, 6.43],
+    }
 
-    # for key, value in super_dict.items():
-    #     print(key, "-", value)
+    for key, value in super_dict.items():
+        print(key, "-", value)
 
-    # for i in super_list:
-    #     print(i["firstname"], "-", i["lastname"])
+    for i in super_list:
+        print(i["firstname"], "-", i["lastname"])
     
 
 
