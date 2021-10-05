2975d73d144a615c9cb717239772074fcfe5a6af
Fabian Tovar <thewolf3shadow@gmail.com>
2021-10-04 21:04:48 -0500

    se modifica y se agrega el mismo documento en otro formato para las pruebas de lectura y graficado con pandas

--- Estadistica descriptiva/prueba.py
+++ Estadistica descriptiva/prueba.py
@@ -1,21 +1,14 @@
 def run():
-    import csv
     import matplotlib.pyplot as plt
     import pandas as pd
-    import numpy as np
     import seaborn as sns
+    from mpl_toolkits.mplot3d import Axes3D
+
+    df = pd.read_csv('2016.csv')
     
-    arch = "2015.csv"
-    with open(arch, newline="" ) as f:
-        datos = csv.reader(f)
-        
-        paises = list(id)
-        
-    print("El tipo de datos de la lista paises es:", type(paises))
-"""""
-    df = pd.read_csv('2015.csv')
-    sns.scatterplot(data = df, x = "Economy (GDP per Capita)", y = "Happyness Score")
-    plt.show()"""
+    sns.scatterplot(data = df, x = "Economy (GDP per Capita)", y = "Happiness Score", hue = "Region", size = "Freedom")
+
+    plt.show()
     
 if __name__ == '__main__':
     run()
