0346b0b61d445bd2f13d59613b01b8aa7f928f73
fabian tovar <thewolf3shadow@gmail.com>
2022-02-03 19:40:11 -0500

    Se agrega nuevo proyecto con conexion a progresSQL

--- Base_datos/BD2_accesi_datos/PersonaDao.py
+++ Base_datos/BD2_accesi_datos/PersonaDao.py
@@ -14,16 +14,60 @@ class PersonaDAO:
 
     @classmethod
     def seleccionar(cls):
-        with Conexion.obtenerCursor() as cursor:
-            cursor.execute(cls._SELECCIONAR)
-            registros = cursor.fetchall()
-            personas = []
-            for registro in registros:
-                persona = Persona(registro[0], registro[1], registro[2], registro[3])
-                personas.append(persona)
-            return personas
+        with Conexion.obtenerConexion() as conexion:
+            with conexion.cursor() as cursor:
+                cursor.execute(cls._SELECCIONAR)
+                registros = cursor.fetchall()
+                personas = []
+                for registro in registros:
+                    persona = Persona(registro[0], registro[1], registro[2], registro[3])
+                    personas.append(persona)
+                return personas
+
+    @classmethod
+    def insertar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email)
+                cursor.execute(cls._INSERTAR, valores)
+                log.debug(f'Persona insertada: {persona}')
+                return cursor.rowcount
+
+    @classmethod
+    def actualizar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email, persona.id_persona)
+                cursor.execute(cls._ACTUALIZAR, valores)
+                log.debug(f'Persona actualizada: {persona}')
+                return cursor.rowcount
+
+    @classmethod
+    def eliminar(cls, persona):
+        with Conexion.obtenerConexion():
+            with Conexion.obtenerCursor() as cursor:
+                valores = (persona.id_persona)
+                cursor.execute(cls._ELIMINAR, valores)
+                log.debug(f'Objeto eliminado: {persona}')
+                return cursor.rowcount
 
 if __name__ == '__main__':
+    # Insertar un registro
+    # persona1 = Persona(nombre='Pedro', apellido='Najera', email='pnajera@mail.com')
+    # personas_insertadas = PersonaDAO.insertar(persona1)
+    # log.debug(f'Personas insertadas: {personas_insertadas}')
+
+    # Actualizar un registro
+    # persona1 = Persona(1,'Juan Carlos', 'Juarez', 'cjjuarez@mail.com')
+    # personas_actualizadas = PersonaDAO.actualizar(persona1)
+    # log.debug(f'Personas actualizadas: {personas_actualizadas}')
+
+    # Eliminar un registro
+    persona1 = Persona(1)
+    personas_eliminadas = PersonaDAO.eliminar(persona1)
+    log.debug(f'Personas eliminadas: {personas_eliminadas}')
+
+    # Seleccionar objetos
     personas = PersonaDAO.seleccionar()
     for persona in personas:
         log.debug(persona)
